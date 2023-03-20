489442d2bf42a2bdafd9c238fadf4157db3bb8ef
francaparroz21 <francaparroz21@gmail.com>
2023-03-20 12:45:04 -0300

    terminando mvp

--- app/src/main/java/com/example/pil_hello_world/activity/MainActivity.kt
+++ app/src/main/java/com/example/pil_hello_world/activity/MainActivity.kt
@@ -9,11 +9,14 @@ import com.example.pil_hello_world.mvp.view.MainView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var presenter: MainContract.Presenter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        presenter = MainPresenter(MainView(this))
         binding.buttonShowText.setOnClickListener {
+            presenter.onClickButtonShow(binding.insertText.text.toString())
         }
     }
 }
